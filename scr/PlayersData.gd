ba179a56783104b3569ad7b2d8b2a1e896946179
esbudylin <budylin.egor@gmail.com>
2023-04-15 17:07:21 +0300

    Update PlayersData.gd
    getter for chess_type allows to play 'board' scene separately during debugiing

--- scr/PlayersData.gd
+++ scr/PlayersData.gd
@@ -7,9 +7,13 @@ var puppet_color
 
 var peer
 
-var chess_type
+var chess_type setget , get_chess_type
 
-func call_config ():
+func get_chess_type():
+	if chess_type: return chess_type
+	else: return call_config().get_value('options', 'chess_type')
+
+func call_config():
 	var config = ConfigFile.new()
 	
 	var err = config.load("res://config.cfg")
